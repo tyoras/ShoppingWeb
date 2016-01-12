cdb3417162d6300e6452fec4ec3931a101b33a50
Yoan <tyoras@gmail.com>
2016-01-12 01:03:00 +0100

    add bootstrap css

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'shopping-app',
-    template: '<h1>Shopping App</h1>'
+    template: `
+    	<div class="container">
+	    	<div class="jumbotron">
+	    		<h1>Shopping App</h1>
+	    		<p>A small web application to manage and share shopping lists<p>
+			</div>
+		</div>
+    `
 })
 export class AppComponent { }
