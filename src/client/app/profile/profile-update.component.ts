e9011e9b6edeeeee972e4fb405eddc70ddee586b
tyoras <tyoras@gmail.com>
2016-10-03 19:10:05 +0200

    change password

--- src/client/app/profile/profile-update.component.ts
+++ src/client/app/profile/profile-update.component.ts
@@ -17,6 +17,7 @@ export class ProfileUpdateComponent implements OnInit {
 
   errorMessage: string;
   user: User;
+  passwords: string[] = [null, null];
   gravatarId: Int32Array | string;
   loading = false;
 
@@ -53,7 +54,7 @@ export class ProfileUpdateComponent implements OnInit {
 
     this.userService.update(this.user).subscribe(
       response => {
-        this.alertService.success(`User ${this.user.name} profile updated`, true);
+        this.alertService.success(`User ${this.user.name} profile updated successfully`, true);
         this.router.navigate(['/profile']);
       },
       error => {
@@ -64,7 +65,29 @@ export class ProfileUpdateComponent implements OnInit {
     );
   }
 
-  getGravatarId(email: string): Int32Array | string {
+  changePassword() {
+    this.loading = true;
+    let newPassword = this.passwords[0];
+
+    if (newPassword === this.passwords[1]) {
+      this.userService.changePassword(this.user.id, this.passwords[0]).subscribe(
+        response => {
+          this.alertService.success(`User ${this.user.name} password changed successfully`, true);
+          this.router.navigate(['/profile']);
+        },
+        error => {
+          //TODO handle 409 conflict status
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      );
+    } else {
+      this.alertService.error("Please type the new password twice.");
+      this.loading = false;
+    }
+  }
+
+  private getGravatarId(email: string): Int32Array | string {
     return Md5.hashStr(email);
   }
 
